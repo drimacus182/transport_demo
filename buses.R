25fa0894ef6ea3646f3176a56a5c3a7a7eda6439
Vladislav <drimacus182@gmail.com>
2017-10-12 16:20:45 +0300

    buses.R

--- buses.R
+++ buses.R
@@ -1,6 +1,22 @@
 library(dplyr)
 library(lubridate)
+library(ggplot2)
 
 data <- read.csv("data/buses_day.csv", colClasses=c('integer', 'character', 'numeric', 'numeric', 'integer', 'character', 'integer', 'numeric'))
 
-data <- data %>% mutate(GPS_DATETIME=GPS_DATETIME %>% as.POSIXct)
+data <- data %>% 
+  mutate(GPS_DATETIME=GPS_DATETIME %>% as.POSIXct) %>%
+  mutate(timestamp=as.integer(GPS_DATETIME)) %>% 
+  mutate(minute=round.POSIXt(GPS_DATETIME, 'mins') %>% as.POSIXct) %>% 
+  filter(timestamp >= 1470023165 & timestamp <= 1470030000)
+
+grouped <- data %>% 
+  mutate(timestamp=as.integer(minute)) %>% 
+  group_by(UNIT_NO, timestamp) %>%
+  filter(row_number()==1) %>% 
+  ungroup()
+
+minutes <- seq(1470023160, 1470030000, 60)
+
+
+
